11c6dd7d4972c481d5018fe5db0be871c3d8f00c
Stefanie Anderson <andersonstefanie11@gmail.com>
2019-02-15 13:29:32 +0100

    added Listmenue

--- src/App.js
+++ src/App.js
@@ -1,11 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
+import List from './List';
 
 export default class App extends Component {
+  constructor(){
+    super();
+    this.state ={
+      ListItem:[{
+        title: 'Dashboard 1', done:false,
+        link:'google.de'
+      },
+        {
+        title:'Dashboard 2', done:false,
+        link:'google.de'
+            },
+        {
+        title:'Dashboard 3', done:false,
+        link:'google.de'
+      }
+    ]
+  };
+}
   render() {
     return (
       <div className="App">
-        
+      <List title="DashboardList" items={this.state.ListItem}/> 
       </div>
     );
   }
